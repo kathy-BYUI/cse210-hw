428f87f7e808a25fdc9546333c18d89b30d397c6
kathy-BYUI <cam21026@byui.edu>
2023-02-13 08:06:16 -0500

    fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,46 @@
+public class Fraction 
+
+{
+    private int _top;
+    private int _bottom;
+
+    //Constructores
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }    
+    public Fraction(int wholeNumber){
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom){}
+
+    //Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+     public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFractionString(){
+        return GetTop() + "/" + GetBottom();
+    }
+
+    public double GetDecimalValue(){
+        double result = ((double)GetTop())/GetBottom();
+        return result;
+    }
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,7 +3,29 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    {        
+        Fraction f1 = new Fraction();
+        f1.SetTop(1);        
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction();
+        f2.SetTop(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction();
+        f3.SetTop(3);
+        f3.SetBottom(4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction();
+        f4.SetTop(1);
+        f4.SetBottom(3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        // Console.WriteLine("{0:N4}", num);
     }
 }
